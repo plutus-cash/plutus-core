b10f890b67b8421ede714f67d7429f07412cb3de
Monty Python <zxmonty@hotmail.com>
2024-11-16 17:22:01 +0300

    lz

--- contracts/interfaces/core/IOReadFacet.sol
+++ contracts/interfaces/core/IOReadFacet.sol
@@ -1,14 +1,15 @@
 //SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 
+import { ILayerZeroEndpointV2, MessagingFee, MessagingReceipt, Origin } from "@layerzerolabs/lz-evm-protocol-v2/contracts/interfaces/ILayerZeroEndpointV2.sol";
+import { AddressCast } from "@layerzerolabs/lz-evm-protocol-v2/contracts/libs/AddressCast.sol";
+
+import { ReadCodecV1, EVMCallComputeV1, EVMCallRequestV1 } from "@layerzerolabs/oapp-evm/contracts/oapp/libs/ReadCodecV1.sol";
+import { OAppOptionsType3 } from "@layerzerolabs/oapp-evm/contracts/oapp/libs/OAppOptionsType3.sol";
+import { OAppRead } from "@layerzerolabs/oapp-evm/contracts/oapp/OAppRead.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+
 interface IOReadFacet {
-
-    uint8 internal constant MAP_ONLY = 0;
-    uint8 internal constant REDUCE_ONLY = 1;
-    uint8 internal constant MAP_AND_REDUCE = 2;
-    uint8 internal constant NONE = 3;
-
-    uint8 private constant READ_MSG_TYPE = 1;
 
     struct EvmReadRequest {
         uint16 appRequestLabel;
@@ -34,7 +35,7 @@ interface IOReadFacet {
         // address poolAddress; // Address of the pool contract
     }
 
-    function getPoolData(uint32 _eid, address _pool) external;
+    function getProportion(uint32 _eid, address _pool, int24[] memory tickRange, bytes calldata _extraOptions) external payable returns (MessagingReceipt memory receipt);
 
     
 }

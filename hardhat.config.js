7986ef5855fc7030f93fa44e227d0a4087e7a324
Monty Python <zxmonty@hotmail.com>
2024-11-16 17:26:28 +0300

    config chains

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,9 @@
 require('hardhat-deploy');
 require("@nomiclabs/hardhat-waffle");
+const dotenv = require('dotenv');
+
+export const ETH_NODE_URI="https://rpc.ankr.com/eth"
+export const BASE_NODE_URI="https://rpc.ankr.com/base"
 
 module.exports = {
   namedAccounts: {
@@ -22,4 +26,18 @@ module.exports = {
       },
     },
   },
+  networks: {
+    hardhat: {
+      forking: {
+          url: BASE_NODE_URI,
+          blockNumber: BLOCK_NUMBER,
+      },  
+    },
+    base: {
+      url: BASE_NODE_URI,
+      chainId: 8453,
+      accounts: [process.env.PK]
+    }
+  }
 };
+
